4899aa1a09989eac244bad25cc986c8fae674cda
maryam-shotunde <maryam.dev.engineer@gmail.com>
2026-04-01 22:15:09 +0100

    feat: setting up with fastapi

---
+++ main.py
@@ -0,0 +1,67 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+books = [
+    {
+        'id': 1,
+        'title' : 'Broke',
+        'author' : 'fatima Rala',
+        'publisher' : 'pratdeep singha',
+        'published_date': '02-04-05',
+        'page count' : 2300,
+        'language': 'Francais'
+    },
+    {
+        'id': 2,
+        'title' : 'As far as the lemon tea grows',
+        'author' : 'fatimata sillah',
+        'publisher' : 'preeta sanyang',
+        'published_date': '01-04-15',
+        'page count' : 1300,
+        'language': 'Espanol'
+    },
+{
+        'id': 3,
+        'title' : 'A thousand yellow sun',
+        'author' : 'chima achebe',
+        'publisher' : 'singh omar',
+        'published_date': '24-04-25',
+        'page count' : 3000,
+        'language': 'portuguese'
+    },
+{
+        'id': 4,
+        'title' : 'NOVEMBER 4TH',
+        'author' : 'COLA HOVIN',
+        'publisher' : 'prithvi singh',
+        'published_date': '03-03-03',
+        'page count' : 6000,
+        'language': 'Anglaise'
+    },
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+]
