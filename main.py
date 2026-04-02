bafb14c14f478ce9280823c7add2dfb004d331cb
maryam-shotunde <maryam.dev.engineer@gmail.com>
2026-04-02 19:36:48 +0100

    feat: working on the bookly reviews III add routes and schemas

--- main.py
+++ main.py
@@ -72,6 +72,13 @@ class Book(BaseModel):
     page_count : int
     language : str
 
+class BookUpdateModel(BaseModel):
+    title : str
+    author: str
+    publisher : str
+    page_count : int
+    language : str
+
 @app.get('/books', response_model=List[Book])
 async def get_all_books():
     return books
@@ -90,9 +97,16 @@ async def get_book(book_id:int) -> dict:
         
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail= 'book not found')
 
-@app.get('/book/{book_id}')
-async def update_book(book_id:int) -> dict:
-    pass
+@app.patch('/book/{book_id}')
+async def update_book(book_id:int, book_update_data:BookUpdateModel) -> dict:
+    for book in books:
+        if book['id'] == book_id:
+            book['title'] == book_update_data.title
+            book['publisher'] == book_update_data.publisher
+            book['page_count'] == book_update_data.page_count
+            book['language'] == book_update_data.language
+            return book
+        raise HTTPException (status_code=status.HTTP_404_NOT_FOUND, detail='book not found')
 
 @app.get('/book/{book_id}')
 async def delete_book(book_id:int) -> dict:
